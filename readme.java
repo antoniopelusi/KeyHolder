b15e5e01bb792cf7a97f068c87b89772789886e0
Antonio Pelusi <antoniopelusi2000@gmail.com>
2021-01-05 21:31:54 +0100

    readme

--- readme.java
+++ readme.java
@@ -1,2 +1,7 @@
-Unzip the src.zip file to find the source code.
-The source code is zipped because of the download problem that edit UTF characters.
+public class Main
+{
+	public static void main(String[] args)
+	{
+		//Unzip the src.zip file to find the source code.
+		//The source code is zipped because of the download problem that edit UTF characters.
+	}
