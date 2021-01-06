c6e54edd473214871eb49631eb24307516990a3e
Antonio Pelusi <antoniopelusi2000@gmail.com>
2021-01-06 18:36:52 +0100

    Delete readme.java

--- readme.java
+++
@@ -1,7 +0,0 @@
-public class Main
-{
-	public static void main(String[] args)
-	{
-		//Unzip the src.zip file to find the source code.
-		//The source code is zipped because of the download problem that edit UTF characters.
-	}
